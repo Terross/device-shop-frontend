7d43954a66016b901c52a692f09aa66c6263a6dc
Terross <dima38091@gmail.com>
2023-04-19 23:36:40 +0300

    Add signUp component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,88 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>Регистрация</v-card-title>
+            <v-card-text>
+                <v-container>
+                    <v-col>
+                        <v-text-field
+                            v-model="firstName"
+                            label="Имя"
+                            required
+                            variant="outlined"
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="lastName"
+                            label="Фамилия"
+                            required
+                            variant="outlined"
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="password"
+                            label="Пароль"
+                            required
+                            variant="outlined"
+                        ></v-text-field>
+                        <v-text-field
+                            v-model="email"
+                            label="Email"
+                            required
+                            variant="outlined"
+                        ></v-text-field>
+                    </v-col>
+                </v-container>
+            </v-card-text>
+            <v-card-actions>
+                <v-container>
+                    <v-btn
+                        color='primary'
+                        variant='flat'
+                        @click="createAccount"
+                    >
+                        Зарегистрироваться
+                    </v-btn>
+                    <v-btn
+                        color='primary'
+                        variant='flat'
+                        @click="logIn"
+                    >
+                        Войти. Аккаунт уже есть
+                    </v-btn>
+                </v-container>     
+            </v-card-actions>
+        </v-card>
+    </v-container>
+    
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+
+export default defineComponent({
+    data() {
+        return {
+            firstName: "",
+            lastName: "",
+            password: "",
+            email: ""
+        }
+    },
+    components: {
+    
+    },
+    methods: {
+        createAccount() {
+            console.log(this.firstName)
+            console.log('Registration clicked')
+        },
+        logIn() {
+            console.log('LogIn')
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const mainTheme = {
+    dark: false,
+    colors: {
+        primary: '#4caf50',
+        secondary: '#8bc34a',
+        accent: '#cddc39',
+        error: '#ff5722',
+        warning: '#ffc107',
+        info: '#3f51b5',
+        success: '#009688'
+        }
+}
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'mainTheme',
+        themes: {
+            mainTheme
+        }
+    }
+})
+
+const app = createApp(App)
+app.use(vuetify)
+app.mount('#app')
